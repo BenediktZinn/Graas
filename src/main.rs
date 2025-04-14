4716dc295056082f6a53f6c76691ca6782e5dc8c
benedikt.zinn@proton.me <benedikt.zinn@proton.me>
2025-04-15 00:30:41 +0200

    Added gtk example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &str = "org.graas.MainUI";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+ 
+    app.run()
+
+}
+fn build_ui(app: &Application){
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Graas")
+        .build();
+
+    window.present();
 }
